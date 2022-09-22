2e1b0fa5f621a30fcf73351b44909dce34d43dca
EC2 Default User <ec2-user@ip-172-31-8-123.ap-northeast-1.compute.internal>
2022-09-22 08:15:25 +0000

    [FINISH]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  #
   devise_for :users
+  # homesのtop-controller
   root to: "homes#top"
+  # postlmageのmodel-controller
+  resources :post_images, only: [:new, :create, :index, :show]
+  #
   get  'homes/about', to: 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_20_081424) do
+ActiveRecord::Schema.define(version: 2022_09_22_080824) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -40,9 +40,11 @@ ActiveRecord::Schema.define(version: 2022_09_20_081424) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "postlmages", force: :cascade do |t|
+  create_table "post_images", force: :cascade do |t|
     t.string "shop_name"
     t.text "caption"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
